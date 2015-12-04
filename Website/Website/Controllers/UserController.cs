f2a6be2ae3f7c1969a61f2e6eb7ba16b684e2c33
gauravshukla1 <gaurav.shukla@colorado.edu>
2015-12-04 13:26:01 -0700

    Code in Mode moved from Controller

--- Website/Website/Controllers/UserController.cs
+++ Website/Website/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 using System.Web.Security;
 
@@ -12,12 +13,12 @@ namespace Website.Controllers
 {
     public class UserController : Controller
     {
-        private string login_type;
-        // _connectionString = @"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\LMS_DB.mdf;Integrated Security = True";
+        //private string login_type;
+        
         // GET: User
         public ActionResult Index()
         {
-            login_type = Convert.ToString(Session["login_type"]).Trim();
+            string login_type = Convert.ToString(Session["login_type"]).Trim();
             if (login_type != null) {
                 if (login_type == "Student") { return RedirectToAction("Index","Student"); }
                 if (login_type == "Librarian") { return RedirectToAction("Index", "Librarian"); }
@@ -37,12 +38,12 @@ namespace Website.Controllers
         [HttpPost]
         public ActionResult LogIn(Models.LibraryUser user)
         {
-            if(IsValid(user.EmailID,user.Password))                            
+            if(user.IsValid(user.EmailID,user.Password))                            
             {
                 FormsAuthentication.SetAuthCookie(user.EmailID, false);
                 /*ViewBag.Message = login_type;
                 return View();*/
-                Session["login_type"] = login_type;
+                Session["login_type"] = user.login_type;
                 Session["EmailID"] = user.EmailID;
                 return RedirectToAction("Index");
             }
@@ -70,7 +71,7 @@ namespace Website.Controllers
         [HttpPost]
         public ActionResult Registration(Models.LibraryUser user)
         {
-            if (StoreUser(user.EmailID, user.Password,user.FirstName,user.LastName))
+            if (user.StoreUser(user.EmailID, user.Password,user.FirstName,user.LastName))
             {
                 return RedirectToAction("Index","User");
             }
@@ -80,147 +81,7 @@ namespace Website.Controllers
             }
             return View();
         }
-        private bool StoreUser(String EmailID, String Password, String FirstName, String LastName)
-        {
-            SqlConnection conn = null;
-            SqlDataReader rdr = null;
-
-            try
-            {
-                if (EmailID != null && Password != null && FirstName != null && LastName != null)
-                {
-                    string connStr = ConfigurationManager.ConnectionStrings["_connectionstring"].ConnectionString;
-                    conn = new SqlConnection(connStr);
-                    conn.Open();
-                    SqlCommand cmd = new SqlCommand("dbo.User_AddStudent", conn);
-                    cmd.CommandType = CommandType.StoredProcedure;
-
-                    // Add the input parameter and set its properties.
-                    SqlParameter parameter1 = new SqlParameter();
-                    parameter1.ParameterName = "@email_id";
-                    parameter1.SqlDbType = SqlDbType.NVarChar;
-                    parameter1.Direction = ParameterDirection.Input;
-                    parameter1.Value = EmailID;
-
-                    // Add the parameter to the Parameters collection. 
-                    cmd.Parameters.Add(parameter1);
-
-                    // Add the input parameter and set its properties.
-                    SqlParameter parameter2 = new SqlParameter();
-                    parameter2.ParameterName = "@Password";
-                    parameter2.SqlDbType = SqlDbType.NVarChar;
-                    parameter2.Direction = ParameterDirection.Input;
-                    parameter2.Value = Password;
-
-                    // Add the parameter to the Parameters collection. 
-                    cmd.Parameters.Add(parameter2);
-
-                    // Add the input parameter and set its properties.
-                    SqlParameter parameter3 = new SqlParameter();
-                    parameter3.ParameterName = "@FirstName";
-                    parameter3.SqlDbType = SqlDbType.NVarChar;
-                    parameter3.Direction = ParameterDirection.Input;
-                    parameter3.Value = FirstName;
-
-                    // Add the parameter to the Parameters collection. 
-                    cmd.Parameters.Add(parameter3);
-
-                    // Add the input parameter and set its properties.
-                    SqlParameter parameter4 = new SqlParameter();
-                    parameter4.ParameterName = "@LastName";
-                    parameter4.SqlDbType = SqlDbType.NVarChar;
-                    parameter4.Direction = ParameterDirection.Input;
-                    parameter4.Value = LastName;
-
-                    // Add the parameter to the Parameters collection. 
-                    cmd.Parameters.Add(parameter4);
-
-                    cmd.ExecuteNonQuery();
-
-                    return true;
-                }
-                else
-                    return false;
-            }
-            finally
-            {
-                if (conn != null)
-                {
-                    conn.Close();
-                }
-                if (rdr != null)
-                {
-                    rdr.Close();
-                }
-            }
-
-            
-  
-            
-        }
-        private bool IsValid(String EmailID, String Password)
-        {
-            //return true;
-            SqlConnection conn = null;
-            SqlDataReader rdr = null;
-           
-            try
-            {
-                conn = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\LMS_DB.mdf;Integrated Security = True");
-                conn.Open();
-                SqlCommand cmd = new SqlCommand("dbo.User_Authenticate", conn);
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                // Add the input parameter and set its properties.
-                SqlParameter parameter1 = new SqlParameter();
-                parameter1.ParameterName = "@email_id";
-                parameter1.SqlDbType = SqlDbType.NVarChar;
-                parameter1.Direction = ParameterDirection.Input;
-                parameter1.Value = EmailID;
-
-                // Add the parameter to the Parameters collection. 
-                cmd.Parameters.Add(parameter1);
-
-                // Add the input parameter and set its properties.
-                SqlParameter parameter = new SqlParameter();
-                parameter.ParameterName = "@Password";
-                parameter.SqlDbType = SqlDbType.NVarChar;
-                parameter.Direction = ParameterDirection.Input;
-                parameter.Value = Password;
-
-                // Add the parameter to the Parameters collection. 
-                cmd.Parameters.Add(parameter);
-
-                // Add the input parameter and set its properties.
-                SqlParameter outparameter = new SqlParameter();
-                outparameter.ParameterName = "@login_type";
-                outparameter.SqlDbType = SqlDbType.NVarChar;
-                outparameter.Size = 12;
-                outparameter.Direction = ParameterDirection.Output;
-                
-
-                // Add the parameter to the Parameters collection. 
-                cmd.Parameters.Add(outparameter);
-
-                cmd.ExecuteNonQuery();
-                login_type = Convert.ToString(cmd.Parameters["@login_type"].Value);
-                
-            }
-            finally
-            {
-                if (conn != null)
-                {
-                    conn.Close();
-                }
-                if (rdr != null)
-                {
-                    rdr.Close();
-                }
-            }
-            if (login_type != null)
-                return true;
-            else
-                return false;
-        }
+        
+        
     }
 }
--- Website/Website/Models/LibraryUser.cs
+++ Website/Website/Models/LibraryUser.cs
@@ -3,17 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Net.Mail;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Website.Models
 {
     public class LibraryUser
     {
-        public string EmailID { get; set; }
-        public string Password { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string login_type { get; set; }
+        private string _EmailID;
+        private string _Password;
+        private string _FirstName;
+        private string _LastName;
+        public string _login_type;
 
+        public string EmailID
+        {
+            get { return _EmailID; }
+            set { this._EmailID = value; } 
+        }
+
+        public string Password
+        {
+            get { return _Password; }
+            set { this._Password = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set { this._FirstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set { this._LastName = value; }
+        }
+
+        public string login_type
+        {
+            get { return _login_type; }
+            set { this._login_type = value; }
+        }
         public String Authenticate(String EmailID,String Password)
         {
             String userType = "";
@@ -69,6 +100,135 @@ namespace Website.Models
             catch (FormatException)
             {
                 return false;
+            }
+        }
+
+        public bool IsValid(String EmailID, String Password)
+        {
+            //return true;
+            SqlConnection conn = null;
+            SqlDataReader rdr = null;
+
+            try
+            {
+                conn = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\LMS_DB.mdf;Integrated Security = True");
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("dbo.User_Authenticate", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("@email_id", SqlDbType.NVarChar).Value = EmailID;
+                cmd.Parameters.Add("@password", SqlDbType.NVarChar).Value = Password;
+
+                // Add the output parameter and set its properties.
+                SqlParameter outparameter = new SqlParameter();
+                outparameter.ParameterName = "@login_type";
+                outparameter.SqlDbType = SqlDbType.NVarChar;
+                outparameter.Size = 12;
+                outparameter.Direction = ParameterDirection.Output;
+
+
+                // Add the parameter to the Parameters collection. 
+                cmd.Parameters.Add(outparameter);
+
+                cmd.ExecuteNonQuery();
+                login_type = Convert.ToString(cmd.Parameters["@login_type"].Value);
+
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+            }
+            if (login_type.Trim() == "Student" || login_type.Trim() == "Librarian" || login_type.Trim() == "Admin")
+                return true;
+            else
+                return false;
+        }
+
+        public bool StoreUser(String EmailID, String Password, String FirstName, String LastName)
+        {
+            SqlConnection conn = null;
+            SqlDataReader rdr = null;
+
+            try
+            {
+                if (EmailID != null && Password != null && FirstName != null && LastName != null)
+                {
+                    // conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["_connectionstring"].ConnectionString);
+                    conn = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\LMS_DB.mdf;Integrated Security = True");
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("dbo.User_AddStudent", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // cmd.Parameters.Add("@email_id", SqlDbType.NVarChar).Value = EmailID;
+                    // cmd.Parameters.Add("@password", SqlDbType.NVarChar).Value = Password;
+                    // Add the input parameter and set its properties.
+                    SqlParameter parameter1 = new SqlParameter();
+                    parameter1.ParameterName = "@email_id";
+                    parameter1.SqlDbType = SqlDbType.NVarChar;
+                    parameter1.Direction = ParameterDirection.Input;
+                    parameter1.Value = EmailID;
+
+                    // Add the parameter to the Parameters collection. 
+                    cmd.Parameters.Add(parameter1);
+
+                    // Add the input parameter and set its properties.
+                    SqlParameter parameter2 = new SqlParameter();
+                    parameter2.ParameterName = "@password";
+                    parameter2.SqlDbType = SqlDbType.NVarChar;
+                    parameter2.Direction = ParameterDirection.Input;
+                    parameter2.Value = Password;
+
+                    // Add the parameter to the Parameters collection. 
+                    cmd.Parameters.Add(parameter2);
+
+                    // Add the input parameter and set its properties.
+                    SqlParameter parameter3 = new SqlParameter();
+                    parameter3.ParameterName = "@firstname";
+                    parameter3.SqlDbType = SqlDbType.NVarChar;
+                    parameter3.Direction = ParameterDirection.Input;
+                    parameter3.Value = FirstName;
+
+                    // Add the parameter to the Parameters collection. 
+                    cmd.Parameters.Add(parameter3);
+
+                    // Add the input parameter and set its properties.
+                    SqlParameter parameter4 = new SqlParameter();
+                    parameter4.ParameterName = "@lastname";
+                    parameter4.SqlDbType = SqlDbType.NVarChar;
+                    parameter4.Direction = ParameterDirection.Input;
+                    parameter4.Value = LastName;
+
+                    // Add the parameter to the Parameters collection. 
+                    cmd.Parameters.Add(parameter4);
+
+                    cmd.ExecuteNonQuery();
+
+                    return true;
+                }
+                else
+                    return false;
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
             }
         }
     }
