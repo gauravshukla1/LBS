39cf997c5909a3a710aa4ba29a0a3b17052f5eb9
gauravshukla1 <gaurav.shukla@colorado.edu>
2015-12-03 17:14:35 -0700

    1

---
+++ Website/Website/Models/LibraryStudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class LibraryStudent
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public string Query { get; set; }
+    }
+}
